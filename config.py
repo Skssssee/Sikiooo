ee48499234263572b52b8fe6544162156cb6ed4e
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-10-05 18:28:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 # 🔄 Git & Update Settings
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/sexyxcoders/Sikio-music")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/Skssssee/Sikiooo")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
